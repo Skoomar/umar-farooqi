ec7ff636d7d4496067b90123be527cc9bc74b5ec
Skoomar <umar_farooqi@hotmail.co.uk>
2020-07-02 23:53:26 +0100

    Initialise new branch to start Reactifying website

---
+++ src/page-layout/Header.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+function Header() {
+    return (
+        <div class="header">
+            <a href="#default" class="logo">Umar Farooqi</a>
+            <div class="header-right">
+                <a class="active" href="#home">Home</a>
+                <a class="#contact">Contact</a>
+                <a class="#about">About</a>
+            </div>
+        </div>
+    )
+}
+
+export default Header
