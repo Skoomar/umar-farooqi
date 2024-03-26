86abad749aaba539d1b44ca15d71585f835dc601
Skoomar <umar_farooqi@hotmail.co.uk>
2024-03-26 23:32:38 +0000

    Having fun with Next Routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return (
-      <h1>Umar Farooqi</h1>
-  );
+    return (
+        <div>
+            <h1>Umar Farooqi</h1>
+            <Link href="/about">About</Link>
+        </div>
+    );
 }
