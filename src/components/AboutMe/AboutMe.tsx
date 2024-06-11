94f2aed0dc4c66ec46dd851a55fbf1a9401e4af8
Umar Farooqi <umar_farooqi@hotmail.co.uk>
2024-06-11 02:00:28 +0100

    Set up master live web page for now

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 const AboutMe = () => (
     <div className="bg-green-400 h-full w-full flex justify-center items-center">
         <div className="z-10">
-            <Image className="mt-40" src="/portrait-alpha.png" width={300} height={300} alt="Portrait" />
-            <p>About Me</p>
+            <Image className="mt-40" src="/portrait.png" width={300} height={300} alt="Portrait" />
+            <h1 className="bg-gray-700 text-white text-4xl">Website under construction...</h1>
         </div>
         <Image className="object-cover" src="/reservoir.jpeg" fill alt="Portrait of author" />
     </div>
