e0427bd9fa428848f50b7d7e1b4ad18dad4056d9
Umar Farooqi <umar_farooqi@hotmail.co.uk>
2024-06-11 01:51:11 +0100

    Move background image to background

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -2,7 +2,10 @@ import Image from "next/image";
 
 const AboutMe = () => (
     <div className="bg-green-400 h-full w-full flex justify-center items-center">
-        <p>About Me</p>
+        <div className="z-10">
+            <Image className="mt-40" src="/portrait-alpha.png" width={300} height={300} alt="Portrait" />
+            <p>About Me</p>
+        </div>
         <Image className="object-cover" src="/reservoir.jpeg" fill alt="Portrait of author" />
     </div>
 );
