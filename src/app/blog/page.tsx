4812be96e9289d302274b6df8b9ac7dfc821710e
Umar Farooqi <umar_farooqi@hotmail.co.uk>
2024-06-23 23:45:43 +0100

    Add Blog route. Refactor some stuff and update README

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,8 @@
+
+const BlogHome = () => (
+  <div>
+      <h1>Blog</h1>
+  </div>
+);
+
+export default BlogHome;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import NavMenu from "@/components/NavMenu/NavMenu";
-import { Separator } from "@/components/ui/separator";
 import AboutMe from "@/components/AboutMe/AboutMe";
 
-export default function Home() {
-    return (
-        <div>
-            <AboutMe />
-        </div>
-    );
-}
+const Home = () => (
+    <div>
+        <AboutMe />
+    </div>
+
+);
+
+export default Home;
