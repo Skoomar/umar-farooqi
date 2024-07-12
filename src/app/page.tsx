d3fdbe24c326f3935fdcd8ddda176c5395cffd9a
Umar Farooqi <umar_farooqi@hotmail.co.uk>
2024-07-12 20:52:54 +0100

    Start laying these blog cards out

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
-import AboutMe from "@/components/AboutMe/AboutMe";
+import Image from 'next/image';
 
 const Home = () => (
     <div>
-        <AboutMe />
+        <div className="bg-reservoir bg-cover h-full w-screen flex justify-center items-center">
+            <div className="my-72">
+                <Image className="mx-auto" src="/portrait.png" width={300} height={300} alt="Author Portrait" />
+                <h1 className="bg-gray-700 text-white text-4xl">Website under construction...</h1>
+            </div>
+        </div>
     </div>
 
 );
--- src/components/blog/posts.tsx
+++ src/components/blog/posts.tsx
@@ -1,43 +1,28 @@
 import Link from 'next/link'
+import { DateTime } from 'luxon';
 import { type Post } from '@/posts';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 
 const BlogCard = ({slug, title, description, publishDate}: {slug: string, title: string, description: string, publishDate: string}) => (
-    // <Card onClick={() => redirect(`/blog/${slug}`)}>
-    <Card>
-        <CardHeader>
-            <CardTitle className="hover:underline"><Link href={`/blog/${slug}`}>{title}</Link></CardTitle>
-            <CardDescription>{publishDate}</CardDescription>
-        </CardHeader>
-        <CardContent>
-            <p>{description}</p>
-        </CardContent>
-    </Card>
+    <Link  href={`/blog/${slug}`}>
+        <Card className="w-60 m-2">
+            <CardHeader>
+                <CardTitle className="hover:underline">{title}</CardTitle>
+                <CardDescription>{publishDate}</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <p>{description}</p>
+            </CardContent>
+        </Card>
+    </Link>
 );
 
 export function Posts({ posts }: { posts: Post[] }) {
     return (
-        <div>
+        <div className="flex">
             {posts.map(({ slug, title, description, publishDate }) => (
                 <BlogCard key={slug} slug={slug} title={title} description={description} publishDate={publishDate} />
             ))}
         </div>
     )
 }
-// export function Posts({ posts }: { posts: Post[] }) {
-//     return (
-//         <ol>
-//             {posts.map(({ slug, title, description, publishDate }) => (
-//                 <li key={slug}>
-//                     <h2>
-//                         <Link href={`/blog/${slug}`}>{title}</Link>
-//                     </h2>
-//                     <p>
-//                         <strong>Published:</strong>{' '}
-//                         {new Date(publishDate).toLocaleDateString()}{' '}
-//                     </p>
-//                 </li>
-//             ))}
-//         </ol>
-//     )
-// }
