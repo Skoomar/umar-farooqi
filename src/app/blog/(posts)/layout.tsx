7cc6558a5ec0f0e79efb84bb785b3ca95d6b26e4
Umar Farooqi <umar_farooqi@hotmail.co.uk>
2024-07-19 02:28:11 +0100

    Mores tyles

--- src/app/blog/(posts)/layout.tsx
+++ src/app/blog/(posts)/layout.tsx
@@ -1,6 +1,6 @@
 export default function BlogPostLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
-        <div className=" px-10 md:px-[20rem] pb-20">
+        <div className=" px-10 py-10 sm:px-20 md:px-32 lg:px-52 lg:py-14 xl:px-80 2xl:px-[30rem]">
             {children}
         </div>
     );
--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,11 +1,10 @@
 import type { MDXComponents } from 'mdx/types';
 import type { ComponentPropsWithoutRef } from 'react';
 import { Separator } from '@/components/ui/separator';
-import Link from 'next/link';
 
 function HeadingOne(props: ComponentPropsWithoutRef<'h1'>) {
     return (
-        <div className="mt-12 mb-10">
+        <div className="mb-10">
             <h1 className="text-4xl text-center mb-3" {...props} />
             <Separator />
         </div>
@@ -15,13 +14,19 @@ function HeadingOne(props: ComponentPropsWithoutRef<'h1'>) {
 function Paragraph(props: ComponentPropsWithoutRef<'p'>) {
     return (
         <p className="mb-5" {...props} />
-    )
+    );
 }
 
 function Hyperlink(props: ComponentPropsWithoutRef<'a'>) {
     return (
         <a className="text-blue-800 font-bold hover:underline" {...props} />
-    )
+    );
+}
+
+function Image(props: ComponentPropsWithoutRef<'img'>) {
+    return (
+        <img {...props} />
+    );
 }
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
@@ -29,6 +34,7 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
         h1: HeadingOne,
         p: Paragraph,
         a: Hyperlink,
+        img: Image,
         ...components,
     };
 }
