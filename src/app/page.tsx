2e4fa52c5e801b7f1446cce75cd4bd722eb740e4
Umar Farooqi <umar_farooqi@hotmail.co.uk>
2024-07-30 17:32:08 +0100

    Image Markdown

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,12 @@ import Image from 'next/image';
 
 const Home = () => (
     <div>
-        <div className="bg-reservoir bg-cover h-full w-screen flex justify-center items-center">
+        <section className="bg-reservoir bg-cover h-full w-screen flex justify-center items-center">
             <div className="my-72">
                 <Image className="mx-auto" src="/portrait.png" width={300} height={300} alt="Author Portrait" />
                 <h1 className="bg-gray-700 text-white text-4xl font-headingSans">Website under construction...</h1>
             </div>
-        </div>
+        </section>
     </div>
 
 );
--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,6 +1,7 @@
 import type { MDXComponents } from 'mdx/types';
 import type { ComponentPropsWithoutRef } from 'react';
 import { Separator } from '@/components/ui/separator';
+import Image from 'next/image';
 
 function HeadingOne(props: ComponentPropsWithoutRef<'h1'>) {
     return (
@@ -11,30 +12,19 @@ function HeadingOne(props: ComponentPropsWithoutRef<'h1'>) {
     );
 }
 
-function Paragraph(props: ComponentPropsWithoutRef<'p'>) {
+function Img(props: ComponentPropsWithoutRef<'img'>) {
     return (
-        <p className="mb-5" {...props} />
-    );
-}
-
-function Hyperlink(props: ComponentPropsWithoutRef<'a'>) {
-    return (
-        <a className="text-blue-600 font-bold hover:underline" {...props} />
-    );
-}
-
-function Image(props: ComponentPropsWithoutRef<'img'>) {
-    return (
-        <img {...props} />
+        <Image src={props.src} alt={props.alt} width="300" height="300" />
     );
 }
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
     return {
         h1: HeadingOne,
-        p: Paragraph,
-        a: Hyperlink,
-        img: Image,
+        h2: (props) => <h2 className="text-xl font-bold mb-1" {...props} />,
+        p: (props) => <p className="mb-5" {...props} />,
+        a: (props) => <a className="text-blue-600 font-bold hover:underline" {...props} />,
+        img: Img,
         ...components,
     };
 }
