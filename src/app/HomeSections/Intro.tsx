fb116e9640bda9d43d70ac23ee2d99c9cd2b0931
Umar Farooqi <umar_farooqi@hotmail.co.uk>
2025-02-17 23:10:18 +0000

    Update portrait

--- src/app/HomeSections/Intro.tsx
+++ src/app/HomeSections/Intro.tsx
@@ -22,7 +22,7 @@ const Intro = () => {
         <header className="bg-cover max-md:bg-[center_right_-17rem] lg:bg-center" style={{ backgroundImage }}>
             <div className="py-16 px-8 lg:px-[10%] bg-black bg-opacity-5">
                 <div className="mb-48 max-lg:text-center lg:mt-6 lg:flex lg:flex-row-reverse">
-                    <Image src="/portrait.webp" alt="Portrait of website author" width={150} height={150}
+                    <Image src="/portrait.png" alt="Portrait of website author" width={150} height={150}
                            className="mx-auto max-lg:mb-7 lg:ml-auto lg:mr-0 border-white border-2 rounded-[50%] shadow-[0_3px_10px_3px_rgba(100,100,100,1)]" />
                     <div className="lg:my-auto">
                         <h1 className="mb-2 text-4xl lg:text-5xl text-white font-semibold text-stroke">UMAR FAROOQI</h1>
